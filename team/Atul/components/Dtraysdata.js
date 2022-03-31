2ed275dbe0b8f67caa82b5a69356591c98459f3e
AtulKumar69 <99542817+AtulKumar69@users.noreply.github.com>
2022-03-31 16:21:09 +0530

    Vases Sculptures Decorative trays
    Using HTML,CSS,Js

---
+++ team/Atul/components/Dtraysdata.js
@@ -0,0 +1,121 @@
+var decorativeTrays=[
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/af824411-62e5-4784-89da-e0e89077ed50/svn/gold-home-decorators-collection-decorative-trays-p170318xx-e4_400.jpg`,
+        title:`Gold Metal Decorative Round Mirror Tray `,
+        auth: `by Home Decorators Collection`,
+        rate:`⭐⭐⭐⭐⭐(85)`,
+        price:`59.25`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/c5532798-178e-4c30-a320-ab1d227ca61d/svn/multi-colored-home-decorators-collection-decorative-trays-tr1812324-nblk-e4_400.jpg`,
+        title:`Black, White and Natural Seagrass Decorative Round Tray (set of 2)`,
+        auth: `by Home Decorators Collection`,
+        rate:`⭐⭐⭐⭐(10)`,
+        price:`41.58`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/f55b10f5-7bdc-4bed-86de-fc6815d9d303/svn/white-litton-lane-decorative-trays-68765-64_400.jpg`,
+        title:`White Metal Farmhouse 2-tier Server `,
+        auth: `by Litton Lane`,
+        rate:`⭐⭐⭐⭐(2)`,
+        price:`79.00`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/8ee492f5-f18f-40a1-a174-841267f3267e/svn/chrome-elegant-designs-decorative-trays-hg1010-chr-64_400.jpg`,
+        title:`10.5 in. Chrome Elipse Crystal Decorative Mirrored Jewelry or Makeup Vanity Organizer Trey`,
+        auth: `by Elegant Designs`,
+        rate:`⭐⭐⭐⭐⭐(11)`,
+        price:`See low Price in Cart`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/bcd65ff2-64a4-4b00-98c7-00510e19d6de/svn/gold-stratton-home-decor-decorative-trays-s36880-64_400.jpg`,
+        title:`Gold Round Tray with Mirror Inlay`,
+        auth: `by Stratton Home Decor`,
+        rate:`⭐⭐⭐⭐(1)`,
+        price:`64.93`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/345c9de8-2204-49c5-8657-88838cd3773c/svn/brown-home-decorators-collection-decorative-trays-v192059xx-e4_400.jpg`,
+        title:`Natural Wood Decorative Octagonal Tray (set of 2)`,
+        auth: `by Home Decorators Collection`,
+        rate:`⭐⭐⭐⭐(10)`,
+        price:`47.40`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/0a1e13a7-f521-4183-b942-16916e55ac74/svn/white-kate-and-laurel-decorative-trays-214400-64_400.jpg`,
+        title:`Marmora White Decorative Tray`,
+        auth: `by Kate and Laurel `,
+        rate:`⭐⭐⭐⭐⭐(3)`,
+        price:`61.20`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/9dfb8416-5802-509f-8f54-f2be4e00823e/svn/white-stratton-home-decor-decorative-trays-s36894-64_400.jpg`,
+        title:`2-Tier Round White and Natural Wood Tray `,
+        auth: `by Stratton Home Decor`,
+        rate:``,
+        price:`99.56`,
+        pickup:`X Pickup Unavailable at Gaum - Tamuning`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/00a220b7-8ceb-468b-8388-a64e947bb86d/svn/multi-colored-home-decorators-collection-decorative-trays-tr1711413-nblk-e4_400.jpg`,
+        title:`Black and Natural Water Hyacinth Decorative Rectangle Tray ( Set of 3)`,
+        auth: `by Home Decorators Collection`,
+        rate:`⭐⭐⭐⭐⭐(12)`,
+        price:`53.40`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/9e17e9cf-982c-50c1-b70b-9f631ed91ccc/svn/natural-wood-stratton-home-decor-decorative-trays-s42548-64_400.jpg`,
+        title:`Farmhouse Rectangle 2-Tiered Decorative Tray Stand`,
+        auth: `by Stratton Home Decor`,
+        rate:`⭐⭐⭐⭐(1)`,
+        price:`101.17`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/8b0eea1d-493b-5d37-a8ec-7ce8d3c723b2/svn/teal-kate-and-laurel-decorative-trays-219559-64_400.jpg`,
+        title:`Lipton Teal decorative Tray`,
+        auth: `by Kate and Laural`,
+        rate:``,
+        price:`69.99`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+
+    },
+    {
+        imgUrl:`https://images.thdstatic.com/productImages/fada72a1-cf6d-4bf5-9acb-45742a0e1c41/svn/bronze-lone-elm-studios-decorative-trays-95196ec-64_400.jpg`,
+        title:`16.34 in. D Broze Round Metal Trays Large Measures (Set of 2)`,
+        auth: `by Lone Elm Studios`,
+        rate:``,
+        price:`53.53`,
+        pickup:`X Pickup Not sold in stores`,
+        delivery:`X Delivery Not available in Guam`,
+    },
+
+]
+localStorage.setItem("decorative",JSON.stringify(decorativeTrays))
