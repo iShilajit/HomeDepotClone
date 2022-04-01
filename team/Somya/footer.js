96e035b91d166c8564879f5988af52caa8174ff0
soumyah1512 <99666938+soumyah1512@users.noreply.github.com>
2022-04-01 10:19:28 +0530

    Header & Footer
    Header & Footer

---
+++ team/Somya/footer.js
@@ -0,0 +1,120 @@
+const foot = () => {
+    return `<div>
+    <div id="topB">
+        <div>
+            <h3>How doers get more done<sup style="font-size:8px">TM</sup></h3>
+        </div>
+        <div>
+            <p>Need Help? Please call us at: <b>1-800-466-3377</b></p>
+        </div>
+    </div>
+    <div id="secB">
+        <div>
+            <ul>
+                <li>Customer Service</li>
+                <li>Check Order Status</li>
+                <li>Pay Your Credit Card</li>
+                <li>Order Cancellation</li>
+                <li>Returns</li>
+                <li>Shipping & Delivery</li>
+                <li>Product Recalls</li>
+                <li>Help & FAQs</li>
+                <li>Privacy & Security Center</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>Resources</li>
+                <li>Specials & Offers</li>
+                <li>Military Discount Benefit</li>
+                <li>DIY Projects & Ideas</li>
+                <li>Truck & Tool Rental</li>
+                <li>Installation & Services</li>
+                <li>Moving Supplies & Rentals</li>
+                <li>Protection Plans</li>
+                <li>Rebate Center</li>
+                <li>Gift Cards</li>
+                <li>Catalog</li>
+                <li>Subscriptions</li>
+            </ul>
+        </div>
+        <div>
+            <ul>
+                <li>About Us</li>
+                <li>Careers</li>
+                <li>Corporate Information</li>
+                <li>Digital Newsroom</li>
+                <li>Home Depot Foundation</li>
+                <li>Investor Relations</li>
+                <li>Investor Relations</li>
+                <li>Suppliers & Providers</li>
+                <li>Affiliate Program</li>
+                <li>Eco Actions</li>
+                <li>Corporate Responsibility</li>
+                <li>Home Depot Licensing Information</li>
+            </ul>
+        </div>
+        <div>
+            <div id="smlBox">
+                <ul>
+                    <li><p><i class="bi bi-credit-card-fill"></i> Special Financing Available everyday*</p></li>
+                    <li><p>Pay & Manage Your Card</p></li>
+                    <li><p>Credit Offers</p></li>
+                </ul>
+            </div>
+            <p>Get $5 off when you signup for emails with savings and tips.</p>
+            <div id="enEmId">
+                <input type="text" placeholder="Enter Email Address">
+                <button>GO</button>
+            </div>
+            <div id="socIc">
+                <i class="bi bi-meta"></i>
+                <i class="bi bi-twitter"></i>
+                <i class="bi bi-pinterest"></i>
+                <i class="bi bi-blockquote-left"></i>
+                <i class="bi bi-youtube"></i>
+                <i class="bi bi-phone-fill"></i>
+            </div>
+        </div>
+    </div>
+    <div id="brands">
+        <p>Shop Our Brands</p>
+        <div id="blogos">
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/company-store.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/blinds-dot-com.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/home-decorator.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/hampton-bay.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/husky.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/lifeproof_v1.svg" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/glacier-bay.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/everbilt.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/style-well.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/hdx.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/vigoro.svg" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/traffic-master.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/defiant.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/eco-smart.png" alt="brand_logo"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/home-accent.png" alt="brand_logo" style="width:40%"></div>
+            <div><img src="https://assets.thdstatic.com/images/v1/brand-logos/legend-force.png" alt="brand_logo"></div>
+        </div>
+    </div>
+    <div id="botF">
+        <p>© 2000-2022 Home Depot. All Rights Reserved. Use of this site is subject to certain Terms Of Use.</p>
+        <p>Local store prices may vary from those displayed. Products shown as available are normally stocked but inventory levels cannot be guaranteed/p>
+        <p>For screen reader problems with this website, please call 1-800-430-3376 or text 38698 (standard carrier rates apply to texts)</p>
+    </div>
+    <div id="botL">
+        <p>Privacy & Security Statement</p>
+        <p>Cookie Usage</p>
+        <p>My Preference Center</p>
+        <p>California Privacy Rights & Report</p>
+        <p>Do Not Sell My Personal Information</p>
+        <p>California Supply Chain Act</p>
+        <p>Site Map</p>
+        <p>Store Directory</p>
+        <p>Mobile Site</p>
+    </div>
+</div>`
+}
+
+export default foot
