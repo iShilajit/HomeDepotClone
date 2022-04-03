3f25059c9c8dd7ae100af92c8d39a64762878e91
Shilajit <shilajit2014paul@gmail.com>
2022-04-03 11:46:36 +0530

    seach
    search nav add

--- team/shilajit/scripts/search.js
+++ team/shilajit/scripts/search.js
@@ -1,14 +1,12 @@
 
-import nav from "/team/Somya/navbar.js"
-let da = nav()
+
+
 let all=JSON.parse(localStorage.getItem("alldata"))||[];
-let inp=document.getElementById("searchInput");
 
-inp.addEventListener("onkeyup",()=>{
-    search()
-})
 
-const search=()=>{
+
+
+const inp=()=>{
     let searchInput=document.getElementById("searchInput").value;
     console.log(searchInput)
     }
